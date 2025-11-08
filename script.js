07e2b2ff8122377ea4f189c107ed47407e9611cc
annamci <annamaymcinnes@icloud.com>
2025-11-08 18:46:11 +0000

    Q1

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-//Q0
+//Q0 Create a web page and external JavaScript file to show the message "I'm JavaScript!". Open the page, ensure that the alert works.
 alert("I'm JavaScript!");
+
+//Q1 Declare two variables: admin and name, Assign the value "John" to name, Copy the value from name to admin, Show the value of admin using alert (must output "John").
+let Admin, Name;
+Name = "John";
+Admin ="name";
+alert( Admin ); // "John"
