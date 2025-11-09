a718a4469cae3f0f633a3b005e9b9ffa075bbb21
annamci <annamaymcinnes@icloud.com>
2025-11-09 18:06:56 +0000

    Q5

--- script.js
+++ script.js
@@ -23,7 +23,48 @@ alert( Name2 );
 
 //Q5 What are the final values of all variables a, b, c and d after the code below?
 let a = 1, b = 1;
-let c = ++a; // 
-let d = b++; // 
+let c = ++a;  
+let d = b++;  
 
-//Q6 
+alert( c ); // 2
+alert( d ); // 1
+alert( a ); // 2
+alert( b ); // 2
+
+//Q6 What are the values of a and x after the code below?
+let A = 2;
+let x = 1 + (a *= 2); 
+alert( a ); // 4
+alert( x ); // 5
+
+//Q7 What are results of these expressions? **
+"" + 1 + 0 
+"" - 1 + 0 
+true + false 
+6 / "3"
+"2" * "3"
+4 + 5 + "px"
+"$" + 4 + 5
+"4" - 2
+"4px" - 2
+"  -9  " + 5
+"  -9  " - 5
+null + 1
+undefined + 1
+" \t \n" - 2
+
+//Q8 Here is some code that asks the user for two numbers and shows their sum. It works incorrectly. The output in the example below is 12 (for default prompt values). Why? Fix it. The result should be 3.
+let AA = prompt("First number?", 1);
+let BB = prompt("Second number?", 2);
+alert(a + b); // 12
+
+//Q9 What will be the result for these expressions?
+5 > 4 --> true
+"apple" > "pineapple"
+"2" > "12"
+undefined == null
+undefined === null
+null == "\n0\n"
+null === +"\n0\n"
+
+//
